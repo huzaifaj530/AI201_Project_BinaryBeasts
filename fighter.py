273216cf2f08ecb59b1ed327ec5c6d799593a6af
Huzaifa <huzaifaj530@gmail.com>
2022-12-23 18:22:09 +0500

    attacking mechanics

--- fighter.py
+++ fighter.py
@@ -5,8 +5,10 @@ class Fighter():
         self.rect=pygame.Rect((x,y,80,180))
         self.vel_y=0
         self.jump=False
+        self.attacking=False
+        self.attack_type=0
 
-    def move(self,screen_width,screen_height):
+    def move(self,screen_width,screen_height,surface,target):
         SPEED=10
         GRAVITY=2
         dx=0
@@ -15,16 +17,27 @@ class Fighter():
         #get key presses
         key=pygame.key.get_pressed()
         
-        #movement
-        if key[pygame.K_a]:
-            dx=-SPEED
-        if key[pygame.K_d]:
-            dx=SPEED
+        #can only move if not attacking
+        if self.attacking==False:
+            #movement
+            if key[pygame.K_a]:
+                dx=-SPEED
+            if key[pygame.K_d]:
+                dx=SPEED
 
-        #jump
-        if key[pygame.K_w] and self.jump==False:
-            self.vel_y=-30
-            self.jump=True
+            #jump
+            if key[pygame.K_w] and self.jump==False:
+                self.vel_y=-30
+                self.jump=True
+            #attack
+            if key[pygame.K_r] or key[pygame.K_t]:
+                self.attack(surface,target)
+                #determine attack type
+                if key[pygame.K_r]:
+                    self.attack_type=1
+                if key[pygame.K_t]:
+                    self.attack_type=2
+            
         #apply gravity
         self.vel_y+=GRAVITY
         dy+=self.vel_y
@@ -43,6 +56,14 @@ class Fighter():
         #update player position
         self.rect.x+=dx
         self.rect.y+=dy
+
+    def attack(self,surface,target):
+        self.attacking=True
+        attacking_rect=pygame.Rect(self.rect.centerx,self.rect.y,2*self.rect.width,self.rect.height)
+        if attacking_rect.colliderect(target.rect):
+            print("hit")
+
+        pygame.draw.rect(surface,(0,255,0),attacking_rect)
 
     def draw(self,surface):
         pygame.draw.rect(surface,(255,0,0),self.rect)
